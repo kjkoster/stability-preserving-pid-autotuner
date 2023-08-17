41559f45c440070a718a63e8e8b55f64f0767a86
Kees Jan Koster <kjkoster@kjkoster.org>
2023-08-17 07:48:18 +0200

    Simplify the initialistion.

--- ddpg_torch.py
+++ ddpg_torch.py
@@ -6,22 +6,18 @@ import torch.optim as optim
 import numpy as np
 
 class OUActionNoise(object):
-    def __init__(self, mu, sigma=0.15, theta=0.2, dt=1e-2, x0=None):
+    def __init__(self, mu, sigma=0.15, theta=0.2, dt=1e-2):
         self.mu = mu
         self.sigma = sigma
         self.theta = theta
         self.dt = dt
-        self.x0 = x0
-        self.reset()
+        self.x_prev = np.zeros_like(self.mu)
 
     def __call__(self):
         x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
             self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
         self.x_prev = x
         return x
-
-    def reset(self):
-        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
 
 
 class ReplayBuffer(object):
